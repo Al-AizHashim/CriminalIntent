7f8179f7b788dfc9d674c5ec51d2d77e2e844c93
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-10-20 02:44:14 +0300

    Enhancing the performance through using DiffUtil

--- app/src/main/java/com/alaizhashim/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/alaizhashim/criminalintent/CrimeListFragment.kt
@@ -1,35 +1,46 @@
 package com.alaizhashim.criminalintent
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import android.text.format.DateFormat
+import androidx.lifecycle.Observer
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import java.text.DateFormat.FULL
 import java.text.SimpleDateFormat
 import java.util.*
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
+    /**
+     * Required interface for hosting activities
+     */
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    } private var callbacks: Callbacks? = null
+    @SuppressLint("SimpleDateFormat")
     var formatter: SimpleDateFormat=SimpleDateFormat ("EEEE, MMM d, yyyy")
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = null
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,11 +50,26 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView =
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-        updateUI()
+        crimeRecyclerView.adapter = adapter
         return view
     }
-    private fun updateUI() {
-        val crimes = crimeListViewModel.crimes
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer {crimes ->
+                crimes?.let {
+                    //Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+
+                }
+            })
+    }
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+    private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
@@ -62,6 +88,7 @@ class CrimeListFragment : Fragment() {
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
         init {
             itemView.setOnClickListener(this)
         }
@@ -69,11 +96,15 @@ class CrimeListFragment : Fragment() {
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text =formatter.format(this.crime.date)
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
-                .show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
     ////////////////////////////////////////////////////////////////////////
@@ -81,6 +112,7 @@ class CrimeListFragment : Fragment() {
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
         private val contact_police_button: Button = itemView.findViewById(R.id.contact_police_button)
         init {
             itemView.setOnClickListener(this)
@@ -93,25 +125,34 @@ class CrimeListFragment : Fragment() {
             contact_police_button.apply {
                 text="Contact Police"
             }
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+            contact_police_button.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
-                .show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
     ///////////////////////////////////////////
-    private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<MainHolder>()  {
+    private inner class CrimeAdapter(var crimes: List<Crime>): ListAdapter<Crime, MainHolder>(CrimeDiffUtil()) {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
                 : MainHolder {
             //  var  view:View
             when(viewType) {
                 1-> {
-                    var view=layoutInflater.inflate(R.layout.list_item_serious_crime, parent, false)
+                    val view=layoutInflater.inflate(R.layout.list_item_serious_crime, parent, false)
                     return SeriousCrimeHolder(view)
                 }
                 else -> {
-                    var view=layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+                    val view=layoutInflater.inflate(R.layout.list_item_crime, parent, false)
                     return CrimeHolder(view)
                 }
             }
@@ -125,12 +166,23 @@ class CrimeListFragment : Fragment() {
 
         override fun getItemViewType(position: Int): Int {
             val x:Int
-            if (crimes[position].requiresPolice)
+           // if (!crimes[position].isSolved)
+            if (false)
                 x=1
             else
                 x=0
             return x
         }
+    }
+    private inner class CrimeDiffUtil:DiffUtil.ItemCallback<Crime>(){
+        override fun areItemsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Crime, newItem: Crime): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
 }
